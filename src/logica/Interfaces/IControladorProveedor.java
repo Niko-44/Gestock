2ed3582377be8dca4f06a7bb77ab2c1aec3cd59a
Cristian686504 <cristian.renz@estudiantes.utec.edu.uy>
2024-09-05 19:54:09 -0300

    Creación de Interfaz de controlador proveedor con todos los casos de uso

--- src/logica/Interfaces/IControladorProveedor.java
+++ src/logica/Interfaces/IControladorProveedor.java
@@ -2,12 +2,62 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
 package logica.Interfaces;
+
+import java.util.ArrayList;
+import logica.Clases.DtProveedor;
+import logica.Clases.DtDireccion;
 
 /**
  *
- * @author n1c0l
+ * @author Cristian
  */
-public class IControladorProveedor {
+public interface IControladorProveedor {
     
+public abstract void ingresarDatosProveedor(DtProveedor datos);
+
+public abstract void confirmarAgregadoProveedor();
+
+public abstract void cancelarAgregadoProveedor();
+
+public abstract void ingresarDatosDireccion(DtDireccion datos);
+
+public abstract void cancelarAgregadoDireccion();
+
+public abstract void confirmarAgregadoDireccion();
+
+public abstract ArrayList<DtProveedor> obtenerProveedores();
+
+public abstract void seleccionarProveedor(DtProveedor proveedor);
+
+public abstract void eliminarProveedor(int idProveedor);
+
+public abstract void cancelarProceso();
+
+public abstract ArrayList<DtDireccion> obtenerDirecciones();
+
+public abstract void seleccionarDireccion(DtDireccion direccion);
+
+public abstract void eliminarDireccion(int idDireccion);
+
+public abstract ArrayList<DtProveedor> obtenerProveedor();
+
+public abstract void modificarDatosProveedor(DtProveedor idProveedor);
+
+public abstract void confirmarModificacion();
+
+public abstract void cancelarModificacion();
+
+public abstract ArrayList<DtDireccion> obtenerDireccion();
+
+public abstract void administradorModificaDireccion(DtDireccion idDireccion);
+
+public abstract void buscarPorNombre(String nombre);
+
+public abstract ArrayList<DtProveedor> listarProveedores();
+
 }
