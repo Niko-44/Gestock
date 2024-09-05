5a017f7ce6512cb9aaf2e24e207a1fc55d668ddc
LuguDev <106494315+Lufredy123xd@users.noreply.github.com>
2024-09-05 15:49:30 -0300

    agregando clases-2

---
+++ src/logica/Interfaces/dfsdfsdf.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica.Interfaces;
+
+/**
+ *
+ * @author Luciano
+ */
+public class dfsdfsdf {
+    
+}
