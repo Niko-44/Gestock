314d6368babcbb3cfa469ef7a13938dbb6463a84
Cristian686504 <cristian.renz@estudiantes.utec.edu.uy>
2024-09-05 21:37:54 -0300

    Atributos, constructores, getters y setters de la clase ingresa

--- src/logica/Clases/Ingresa.java
+++ src/logica/Clases/Ingresa.java
@@ -4,10 +4,64 @@
  */
 package logica.Clases;
 
-/**
- *
- * @author n1c0l
- */
+import java.util.Date;
+
 public class Ingresa {
-    
+    private Date fechaIngreso;
+    private int cantidad;
+    private int lote;
+    private float precioCompra;
+    private Proveedor proveedor;
+
+    public Ingresa() {
+    }
+
+    public Ingresa(Date fechaIngreso, int cantidad, int lote, float precioCompra, Proveedor proveedor) {
+        this.fechaIngreso = fechaIngreso;
+        this.cantidad = cantidad;
+        this.lote = lote;
+        this.precioCompra = precioCompra;
+        this.proveedor = proveedor;
+    }
+
+    public Date getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(Date fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getLote() {
+        return lote;
+    }
+
+    public void setLote(int lote) {
+        this.lote = lote;
+    }
+
+    public float getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(float precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
 }
+
