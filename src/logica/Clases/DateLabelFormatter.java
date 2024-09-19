c8e744b7ba7cf5fb763bc66624f4f8b230e19443
Cristian686504 <cristian.renz@estudiantes.utec.edu.uy>
2024-09-19 04:10:38 -0300

    DatePicker funcionando

--- src/logica/Clases/DateLabelFormatter.java
+++ src/logica/Clases/DateLabelFormatter.java
@@ -5,6 +5,7 @@
 package logica.Clases;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import javax.swing.JFormattedTextField;
 /**
@@ -12,18 +13,22 @@ import javax.swing.JFormattedTextField;
  * @author Cristian
  */
 public class DateLabelFormatter extends JFormattedTextField.AbstractFormatter {
-    private String datePattern = "yyyy-MM-dd";
-    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+    private static final long serialVersionUID = 1L;
+    private final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
 
     @Override
     public Object stringToValue(String text) throws ParseException {
-        return dateFormatter.parseObject(text);
+        return dateFormatter.parse(text); // Convierte el texto en un objeto Date
     }
 
     @Override
     public String valueToString(Object value) throws ParseException {
-        if (value != null) {
-            return dateFormatter.format((Date) value);
+        if (value instanceof Calendar) {
+            // Si el valor es una instancia de Calendar, obtenemos la fecha correspondiente
+            Calendar cal = (Calendar) value;
+            return dateFormatter.format(cal.getTime()); // Convertimos la fecha en texto
+        } else if (value instanceof Date) {
+            return dateFormatter.format((Date) value); // Si es Date, lo convertimos directamente a texto
         }
         return "";
     }
