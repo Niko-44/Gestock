72be1192555f6234bf295b6da631bdacaa6966c1
ValentinoVignolo <jesusmart392@gmail.com>
2024-10-17 00:57:12 -0300

    Buscar articulo(carga el proveedor)

--- src/logica/servicios/ArticulosServicios.java
+++ src/logica/servicios/ArticulosServicios.java
@@ -150,7 +150,14 @@ public class ArticulosServicios {
         ArrayList<Articulo> articulos = new ArrayList<>();
 
         try {
-            String sql = "SELECT ARTICULO.*, CATEGORIA.nombre_categoria, (SELECT F.nombre_fabricante FROM fabricante as F JOIN FABRICA AS FA on F.id_fabricante = FA.id_fabricante_fk WHERE FA.id_articulo_fk = articulo.id_articulo) as nombre_fabricante FROM ARTICULO JOIN CATEGORIA ON ARTICULO.id_categoria_fk = CATEGORIA.id_categoria Where LOWER(ARTICULO." + atributo + ") like LOWER('%" + datoABuscar + "%')";
+            String sql = "SELECT ARTICULO.*, CATEGORIA.nombre_categoria, \n" +
+                                                            "	(SELECT F.nombre_fabricante FROM fabricante as F \n" +
+                                                            "		JOIN FABRICA AS FA on F.id_fabricante = FA.id_fabricante_fk \n" +
+                                                            "		WHERE FA.id_articulo_fk = articulo.id_articulo) as nombre_fabricante ,\n" +
+                                                            "	(SELECT P.nombre_proveedor FROM proveedor AS P\n" +
+                                                            "		JOIN ingresa as i on P.id_proveedor = i.id_proveedor_fk\n" +
+                                                            "        WHERE i.id_articulo_fk = articulo.id_articulo) as nombre_proveedor\n" +
+                                                            "FROM ARTICULO JOIN CATEGORIA ON ARTICULO.id_categoria_fk = CATEGORIA.id_categoria Where LOWER(ARTICULO." + atributo + ") like LOWER('%" + datoABuscar + "%')";
 
             if (atributo.equals("SKU")) {
                 sql = "SELECT ARTICULO.*, CATEGORIA.nombre_categoria, (SELECT F.nombre_fabricante FROM fabricante as F JOIN FABRICA AS FA on F.id_fabricante = FA.id_fabricante_fk WHERE FA.id_articulo_fk = articulo.id_articulo) as nombre_fabricante FROM ARTICULO JOIN CATEGORIA ON ARTICULO.id_categoria_fk = CATEGORIA.id_categoria Where ARTICULO." + atributo + " = " + datoABuscar + "";
@@ -163,6 +170,8 @@ public class ArticulosServicios {
             while (rs.next()) {
                 Fabricante fabricante = new Fabricante();
                 fabricante.setNombre(rs.getString("nombre_fabricante"));
+                Proveedor proveedor = new Proveedor();
+                proveedor.setNombre(rs.getString("nombre_proveedor"));
                 Categoria categoria = new Categoria();
                 categoria.setId(rs.getInt("id_categoria_fk"));
                 categoria.setNombre(rs.getString("nombre_categoria"));
@@ -179,6 +188,7 @@ public class ArticulosServicios {
 
                 Articulo articulo = new Articulo(idArticulo, sku, nombre, descripcion, stock, precio, peso, updateDate, createDate, categoria);
                 articulo.setFabricante(fabricante);
+                articulo.setProveedor(proveedor);
                 articulos.add(articulo);
             }
             rs.close();
